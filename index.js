808f19c865797a7b528761995d8c50d42d4072c2
jahidhasan234031 <jahidhasan234031@mail.com>
2022-10-25 16:18:18 +0600

    api running done

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+const courses = require('./courses.json')
+
+app.get('/', (req, res) => {
+    res.send('news api running')
+})
+
+
+
+app.listen(port, () => {
+    console.log('assigment server running port', port)
+})
